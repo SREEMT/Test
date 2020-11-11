711bae016cd8f053cf76b11bbb52ad1bc05db012
TMEERS <73982044+SREEMT@users.noreply.github.com>
2020-11-11 14:46:54 -0700

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,5 +1,5 @@
 
-character_name = "Sebi"
+character_name = "You"
 character_age = "50"
 print("There once was a man named " + character_name + ",")
 print("he was " + character_age + " years old. ")
